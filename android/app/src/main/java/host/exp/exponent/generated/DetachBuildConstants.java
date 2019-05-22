80234041b33444487bd139448bc43279ca009d19
Jacky Chan <React-Native@Jackys-MacBook-Pro.local>
2019-05-22 22:34:38 +0800

    first

---
+++ android/app/src/main/java/host/exp/exponent/generated/DetachBuildConstants.java
@@ -0,0 +1,8 @@
+package host.exp.exponent.generated;
+
+// This file is auto-generated. Please don't rename!
+public class DetachBuildConstants {
+
+  public static final String DEVELOPMENT_URL = "exp1c9cc15b96714d56913cecbf9023c712://192.168.1.101:80";
+
+}
